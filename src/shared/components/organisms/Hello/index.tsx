72ad7d17b90a1c6f2088b8e942a1e4effd819686
hrddd <nuhhunnuhhun@gmail.com>
2020-06-17 03:37:08 +0900

    Add State with Redux
    Devide Presentation from Container

--- src/shared/components/organisms/Hello/index.tsx
+++ src/shared/components/organisms/Hello/index.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { changeVisibility } from "../../../redux/modules/hello";
+import { RootState } from "../../../redux/modules/reducer";
+import Hello from "./Hello";
 
-export default function Hello() {
-  return <div>Hello!</div>;
-}
+export default compose(
+  connect(
+    (state: RootState) => ({
+      isVisible: state.app.hello.isVisible, // store の state の中から、指定した isVisible を props として渡す
+    }),
+    dispatch => ({
+      onChangeVisibility: () => dispatch(changeVisibility()), // changeVisibilityを store に　dispatchする関数を返す
+    }),
+  ),
+)(Hello);
