bd7cef011e0cfba0fcb0d91c428d3150d0c8e9aa
hrddd <nuhhunnuhhun@gmail.com>
2020-06-18 03:54:19 +0900

    Add API

--- src/shared/components/organisms/Hello/index.tsx
+++ src/shared/components/organisms/Hello/index.tsx
@@ -1,6 +1,6 @@
 import { compose } from "redux";
 import { connect } from "react-redux";
-import { changeVisibility } from "../../../redux/modules/hello";
+import { changeVisibility, getComments } from "../../../redux/modules/hello";
 import { RootState } from "../../../redux/modules/reducer";
 import Hello from "./Hello";
 
@@ -8,9 +8,11 @@ export default compose(
   connect(
     (state: RootState) => ({
       isVisible: state.app.hello.isVisible, // store の state の中から、指定した isVisible を props として渡す
+      comments: state.app.hello.comments,
     }),
     dispatch => ({
       onChangeVisibility: () => dispatch(changeVisibility()), // changeVisibilityを store に　dispatchする関数を返す
+      onClickGetComments: () => dispatch(getComments() as any),
     }),
   ),
 )(Hello);
