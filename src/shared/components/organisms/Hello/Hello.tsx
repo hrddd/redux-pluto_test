ee73473360fa87868b719a14d1d24c0dcb64733f
hrddd <nuhhunnuhhun@gmail.com>
2020-06-19 04:34:00 +0900

    Fetch API on landing
    https://github.com/recruit-tech/redux-pluto/wiki/Step3:API%E3%81%8B%E3%82%89%E5%80%A4%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B#%E3%83%9A%E3%83%BC%E3%82%B8%E3%82%92%E9%96%8B%E3%81%8F%E3%82%BF%E3%82%A4%E3%83%9F%E3%83%B3%E3%82%B0%E3%81%A7api%E3%81%AB%E3%83%AA%E3%82%AF%E3%82%A8%E3%82%B9%E3%83%88%E3%82%92%E9%A3%9B%E3%81%B0%E3%81%99
    - 現状に合わせ、redux-async-loader -> @recruit-tech/redux-async-loader に変更
    - まだページ表示時にActionがdispatchされない

--- src/shared/components/organisms/Hello/Hello.tsx
+++ src/shared/components/organisms/Hello/Hello.tsx
@@ -8,18 +8,14 @@ type Props = {
     text: string;
   }[];
   onChangeVisibility: Function;
-  onClickGetComments: Function;
 };
 
 export default function Hello(props: Props) {
-  const { isVisible, onChangeVisibility, comments, onClickGetComments } = props;
+  const { isVisible, onChangeVisibility, comments } = props;
   return (
     <div>
       {isVisible &&
         comments.map(comment => <div key={comment.id}>{comment.text}</div>)}
-      <button type="button" onClick={() => onClickGetComments()}>
-        get comments
-      </button>
       <button type="button" onClick={() => onChangeVisibility()}>
         {isVisible ? "hide" : "show"}
       </button>
--- src/shared/components/organisms/Hello/index.tsx
+++ src/shared/components/organisms/Hello/index.tsx
@@ -1,10 +1,12 @@
 import { compose } from "redux";
 import { connect } from "react-redux";
+import { asyncLoader } from "@recruit-tech/redux-async-loader"; // @recruit-tech/redux-asynch-roder から　asyncLoader　をインポート
 import { changeVisibility, getComments } from "../../../redux/modules/hello";
 import { RootState } from "../../../redux/modules/reducer";
 import Hello from "./Hello";
 
 export default compose(
+  asyncLoader((props, store) => store.dispatch(getComments())),
   connect(
     (state: RootState) => ({
       isVisible: state.app.hello.isVisible, // store の state の中から、指定した isVisible を props として渡す
@@ -12,7 +14,6 @@ export default compose(
     }),
     dispatch => ({
       onChangeVisibility: () => dispatch(changeVisibility()), // changeVisibilityを store に　dispatchする関数を返す
-      onClickGetComments: () => dispatch(getComments() as any),
     }),
   ),
 )(Hello);
